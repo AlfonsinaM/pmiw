54e2148a3eaea418a27c65efe1128b3b0c2bbdd4
Alfonsina M <166077017+AlfonsinaM@users.noreply.github.com>
2024-11-15 21:55:06 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,16 @@
+//Morales Alfonsina 119093/5 y Sarah Recki 119124/4 
+// https://youtu.be/RtbGdQeMDMc
+const Estados = {
+  INICIO: "inicio",
+  JUEGO: "juego",
+  GANADOR: "ganador",
+  PERDEDOR: "perdedor",
+  CREDITOS: "creditos",
+};
+
+const Config = {
+  imgBasura: [],
+  imgFondo: null,
+  imgFondoInicio: null,
+  objetivoPuntos: 3,
+};
